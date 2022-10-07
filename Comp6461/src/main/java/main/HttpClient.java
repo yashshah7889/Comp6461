e0210e9814c25c6cfe64b16eb5438e8e6cd2a670
yashs <yashs@192.168.2.16>
2022-10-07 12:24:20 -0400

    initial setup for request

--- Comp6461/src/main/java/main/HttpClient.java
+++ Comp6461/src/main/java/main/HttpClient.java
@@ -1,7 +1,16 @@
 package main;
 
 public class HttpClient {
+	private String httpRequest;
+	
 	HttpClient(){
 		System.out.println("hii");
+	}
+	public void setHttpRequest(String cURL) {
+		cURL=httpRequest;
+		
+	}
+	public String getHttpRequest() {
+		return httpRequest;
 	}
 }
--- Comp6461/src/main/java/main/main.java
+++ Comp6461/src/main/java/main/main.java
@@ -1,7 +1,22 @@
 package main;
 
+import java.util.Scanner;
+
 public class main {
+	private static final String init = "httpc";
+	private static HttpClient req=new HttpClient(); 
+	
 public static void main(String args[]) {
-	System.out.println("hello world");
+	while(true) {
+		System.out.println("Enetr the command you want to execute.");
+		Scanner sc=new Scanner(System.in);
+		String cURL=sc.nextLine();
+		req.setHttpRequest(cURL);
+	
+		if(req.getHttpRequest()==null || req.getHttpRequest().isEmpty()) {
+			System.out.println("URL not valid please try again");
+			continue;
+		}
+	}
 }
 }
