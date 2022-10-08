a382a97b426a90e1be4cf3dcc73d5d7fe9a25288
rucha1412 <rucha.prajapati98@gmail.com>
2022-10-07 23:42:24 -0400

    initial checking of condition for request

--- Comp6461/src/main/java/main/HttpClient.java
+++ Comp6461/src/main/java/main/HttpClient.java
@@ -42,27 +42,19 @@ public class HttpClient {
 			if(listOfReqData.contains("help")) {
 				if(listOfReqData.contains("get")) {
 					System.out.println("usage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL\r\n"
-							+ "Post executes a HTTP POST request for a given URL with inline data or from\r\n"
-							+ "file.\r\n"
-							+ "-v Prints the detail of the response such as protocol, status,\r\n"
-							+ "and headers.\r\n"
-							+ "-h key:value Associates headers to HTTP Request with the format\r\n"
-							+ "'key:value'.\r\n"
+							+ "Post executes a HTTP POST request for a given URL with inline data or from file.\r\n"
+							+ "-v Prints the detail of the response such as protocol, status, and headers.\r\n"
+							+ "-h key:value Associates headers to HTTP Request with the format 'key:value'.\r\n"
 							+ "-d string Associates an inline data to the body HTTP POST request.\r\n"
-							+ "-f file Associates the content of a file to the body HTTP POST\r\n"
-							+ "request.\r\n"
+							+ "-f file Associates the content of a file to the body HTTP POST request.\r\n"
 							+ "Either [-d] or [-f] can be used but not both.");
 				}else if(listOfReqData.contains("post")) {
 					System.out.println("usage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL\r\n"
-							+ "Post executes a HTTP POST request for a given URL with inline data or from\r\n"
-							+ "file.\r\n"
-							+ "-v Prints the detail of the response such as protocol, status,\r\n"
-							+ "and headers.\r\n"
-							+ "-h key:value Associates headers to HTTP Request with the format\r\n"
-							+ "'key:value'.\r\n"
+							+ "Post executes a HTTP POST request for a given URL with inline data or from file.\r\n"
+							+ "-v Prints the detail of the response such as protocol, status, and headers.\r\n"
+							+ "-h key:value Associates headers to HTTP Request with the format 'key:value'.\r\n"
 							+ "-d string Associates an inline data to the body HTTP POST request.\r\n"
-							+ "-f file Associates the content of a file to the body HTTP POST\r\n"
-							+ "request.\r\n"
+							+ "-f file Associates the content of a file to the body HTTP POST request.\r\n"
 							+ "Either [-d] or [-f] can be used but not both.");
 				}else {
 					System.out.println("httpc is a curl-like application but supports HTTP protocol only.\r\n"
