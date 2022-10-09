33c8f1da78e0581b0a7cfb15664e49c97a7c2a91
yashs <yashs@192.168.2.16>
2022-10-09 18:07:47 -0400

    correction

--- Comp6461/src/main/java/main/ClientRequest.java
+++ Comp6461/src/main/java/main/ClientRequest.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClientRequest {
@@ -7,16 +8,48 @@ public class ClientRequest {
 	private boolean hasVerbose;
 	private boolean hasHeader;
 	private String url;
-	private List<String> listOfHeaders;
+	private List<String> listOfHeaders=new ArrayList<String>();
 	private boolean hasInlineData;
 	private String inlineData;
 	private String filePath;
 	private boolean transferSucc;
 	private String fileTransferPath;
 	private String requestMethod;
+	private boolean isFileWrite;
+	private String fileWritePath;
+	private boolean isRedirect;
+	private String redirectLocation;
 	
 	ClientRequest(){
 		System.out.println("hello");
+	}
+	
+	public void setRequestUrl(String url) {
+		this.url= url;
+	}
+	public String getRequestUrl() {
+		return url;
+	}
+	
+	public void setInlineData(String inlineData) {
+		this.inlineData= inlineData;
+	}
+	public String getInlineData() {
+		return this.inlineData;
+	}
+	
+	public String getRedirectLocation() {
+		return redirectLocation;
+	}
+	public void setRedirectLocation(String redirectLocation) {
+		this.redirectLocation = redirectLocation;
+	}
+	
+	public void setRequestMethod(String requestMethod) {
+		this.requestMethod=requestMethod;
+	}
+	public String getRequestMethod() {
+		return this.requestMethod;
 	}
 	
 	public void setHttpRequest(String cURL) {
@@ -33,13 +66,6 @@ public class ClientRequest {
 		return this.hasVerbose;
 	}
 	
-	public void setRequestUrl(String url) {
-		this.url= url;
-	}
-	public String getRequestUrl() {
-		return url;
-	}
-	
 	public void setHasHeader(boolean hasHeader) {
 		this.hasHeader= hasHeader;
 	}
@@ -47,13 +73,6 @@ public class ClientRequest {
 		return this.hasHeader;
 	}
 	
-	public void setHeaders(List<String> listOfHeaders) {
-		this.listOfHeaders= listOfHeaders;
-	}
-	public List<String> getHeaderLst() {
-		return listOfHeaders;
-	}
-
 	public void setHasInlineData(boolean hasInlineData) {
 		this.hasInlineData= hasInlineData;
 	}
@@ -61,18 +80,19 @@ public class ClientRequest {
 		return hasInlineData;
 	}
 	
-	public void setInlineData(String inlineData) {
-		this.inlineData= inlineData;
-	}
-	public String getInlineData() {
-		return this.inlineData;
-	}
-	
 	public void setTransferSuc(boolean transferSucc) {
 		this.transferSucc= transferSucc;
 	}
 	public boolean getTransferSuc() {
 		return transferSucc;
+	}
+
+	public boolean isFileWrite() {
+		return isFileWrite;
+	}
+
+	public void setFileWrite(boolean isFileWrite) {
+		this.isFileWrite = isFileWrite;
 	}
 	
 	public void setFileTransferPath(String fileTransferPath) {
@@ -82,19 +102,28 @@ public class ClientRequest {
 		return this.fileTransferPath;
 	}
 	
-	public void setRequestMethod(String requestMethod) {
-		this.requestMethod=requestMethod;
+	public void setFileWritePath(String fileWritePath) {
+		this.fileWritePath=fileWritePath;
 	}
-	public String getRequestMethod() {
-		return this.requestMethod;
+	public String getFileWritePath() {
+		return this.fileWritePath;
+	}
+	
+	public boolean isRedirect() {
+		return isRedirect;
 	}
 
-	public String getFilePath() {
-		return this.filePath;
+	public void setRedirect(boolean isRedirect) {
+		this.isRedirect = isRedirect;
 	}
-	public void setFilePath(String filePath) {
-		this.filePath = filePath;
+	
+	public void setHeaders(List<String> listOfHeaders) {
+		this.listOfHeaders= listOfHeaders;
 	}
+	public List<String> getHeaderLst() {
+		return listOfHeaders;
+	}
+
 
 
 
--- Comp6461/src/main/java/main/HttpClient.java
+++ Comp6461/src/main/java/main/HttpClient.java
@@ -22,20 +22,20 @@ public class HttpClient {
 	ClientRequest req= new ClientRequest();
 	int track=0;
 	
-	List<String> listOfHeaders= new ArrayList<String>();
-	
+	private static List<String> listOfHeaders= null;
 	private static Socket socket=null;
-	
 	private static StringBuilder fd = null;
 	
 	HttpClient(){
 		System.out.println("hii");
 	}
 	public void processRequest(String command) throws URISyntaxException, UnknownHostException, IOException{
-		fd = new StringBuilder();
 		
+		boolean flag=true;
 		// to enter the command till the right command not entered.
-		while(true){
+		while(flag){
+			fd = new StringBuilder();
+			listOfHeaders =new ArrayList<String>();
 			if(track==0) {
 				req.setHttpRequest(command);
 				if(req.getHttpRequest()==null || req.getHttpRequest().isEmpty()) {
@@ -60,13 +60,12 @@ public class HttpClient {
 			
 			if(listOfReqData.contains("help")) {
 				if(listOfReqData.contains("get")) {
-					System.out.println("usage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL\r\n"
-							+ "Post executes a HTTP POST request for a given URL with inline data or from file.\r\n"
-							+ "-v Prints the detail of the response such as protocol, status, and headers.\r\n"
-							+ "-h key:value Associates headers to HTTP Request with the format 'key:value'.\r\n"
-							+ "-d string Associates an inline data to the body HTTP POST request.\r\n"
-							+ "-f file Associates the content of a file to the body HTTP POST request.\r\n"
-							+ "Either [-d] or [-f] can be used but not both.");
+					System.out.println("usage: httpc get [-v] [-h key:value] URL\r\n"
+							+ "Get executes a HTTP GET request for a given URL.\r\n"
+							+ "-v Prints the detail of the response such as protocol, status,\r\n"
+							+ "and headers.\r\n"
+							+ "-h key:value Associates headers to HTTP Request with the format\r\n"
+							+ "'key:value'.");
 				}else if(listOfReqData.contains("post")) {
 					System.out.println("usage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL\r\n"
 							+ "Post executes a HTTP POST request for a given URL with inline data or from file.\r\n"
@@ -88,13 +87,13 @@ public class HttpClient {
 			}
 			
 			
-			if(listOfReqData.get(0).contains("httpc") && (listOfReqData.get(1).contains("get") || listOfReqData.get(0).contains("post"))) {
+			if(listOfReqData.get(0).contains("httpc") && (listOfReqData.get(1).contains("get") || listOfReqData.get(1).contains("post"))) {
 				if(listOfReqData.get(1).contains("get") && (listOfReqData.contains("-d") || listOfReqData.contains("-f") || listOfReqData.contains("--d"))) {
 					System.out.println("get option should not be used with the options -d or -f");
 					continue;
 				}
 				
-				if(listOfReqData.get(1).contains("post") && listOfReqData.contains("-f") &&  (listOfReqData.contains("-d") || listOfReqData.contains("--d"))) {
+				if(listOfReqData.get(1).contains("post") &&( listOfReqData.contains("-f") &&  (listOfReqData.contains("-d") || listOfReqData.contains("--d")))){
 					System.out.println("post should have either -d or -f but not both");
 					continue;
 				}
@@ -102,17 +101,18 @@ public class HttpClient {
 				parseRequestQuery(listOfReqData);
 				
 				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				String s;
+				//String s;
 				String status = br.readLine();
 				
 				//redirect not done
 				
-				if(req.getTransferSuc()) {
+				if(req.isFileWrite()) {
 					printInFile(br,status);
 					
 					//redirect check;
 					
 				}else {
+					
 					//method call for printing response in console
 					displayResponse(br,status);
 				}
@@ -123,28 +123,35 @@ public class HttpClient {
 			}else {
 				System.out.println("Invalid URL please. Provide valid httpc get or httpc post URL");
 			}
+			flag=false;
 		}
 	}
 
 public void displayResponse(BufferedReader reader, String status) throws IOException {
-		
+	
+	String line;
 		if(req.hasVerbose()) {
+			
 			System.out.println(status);
-			while(reader.readLine()!=null) {
-				System.out.println(reader.readLine());
-				if(reader.readLine().equals("}")) {
+			while((line=reader.readLine()) != null) {
+				System.out.println(line);
+				if(line.equals("}")) {
 					break;
 				}
 			}
 		}else {
+			
 			boolean jsonPresent= false;
-			while(reader.readLine()!=null) {
-				if(reader.readLine().trim().equals("{")) {
+			while((line=reader.readLine()) != null) {
+				//System.out.println("hiuo");
+				System.out.println(line);
+				if(line.trim().equals("{")) {
 					jsonPresent= true;
 				}
 				if(jsonPresent) {
-					System.out.println(reader.readLine());
-					if(reader.readLine().equals("}")) {
+					
+					System.out.println(line);
+					if(line.equals("}")) {
 						break;
 					}
 				}
@@ -155,9 +162,10 @@ public void displayResponse(BufferedReader reader, String status) throws IOExcep
 	
 	public void printInFile(BufferedReader reader, String status) throws IOException {
 		
-		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(req.getFilePath(), true));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(req.getFileWritePath(), true));
 		
 		if(req.hasVerbose()) {
+			bufferedWriter.write(status);
 			System.out.println(status);
 			while(reader.readLine()!=null) {
 				bufferedWriter.write(reader.readLine());
@@ -167,13 +175,13 @@ public void displayResponse(BufferedReader reader, String status) throws IOExcep
 			}
 		}else {
 			boolean jsonPresent= false;
-			while(reader.readLine()!=null) {
-				if(reader.readLine().trim().equals("{")) {
+			while((reader.readLine())!=null) {
+				if((reader.readLine()).trim().equals("{")) {
 					jsonPresent= true;
 				}
 				if(jsonPresent) {
 					bufferedWriter.write(reader.readLine());
-					if(reader.readLine().equals("}")) {
+					if((reader.readLine()).equals("}")) {
 						break;
 					}
 				}
@@ -181,9 +189,10 @@ public void displayResponse(BufferedReader reader, String status) throws IOExcep
 			}
 		}
 		
-		System.out.println("Response has been stored successfully in ( "+ req.getFilePath()+ " ) File path");
-		bufferedWriter.close();
+		System.out.println("Response has been stored successfully in ( "+ req.getFileWritePath()+ " ) File path");
 		bufferedWriter.flush();
+		bufferedWriter.close();
+		
 	}
 	
 	public void parseRequestQuery(List<String> reqData) throws URISyntaxException, UnknownHostException, IOException {
@@ -192,7 +201,7 @@ public void displayResponse(BufferedReader reader, String status) throws IOExcep
 		while(i<reqData.size()) {
 			if(reqData.get(i).equalsIgnoreCase("-v")) {
 				req.setHasVerbose(true);
-			}else if(reqData.get(i).startsWith("http://")||reqData.get(i).startsWith("https://")) {
+			}else if(reqData.get(i).startsWith("http://") || reqData.get(i).startsWith("https://")) {
 				//splitiing of requesting in two parts domain and path after http://
 				req.setRequestUrl(reqData.get(i));
 			}else if(reqData.get(i).equals("-h")) {
@@ -209,33 +218,33 @@ public void displayResponse(BufferedReader reader, String status) throws IOExcep
 			//-o is remaining
 		i++;	
 		}
-		i=0;
+
 		req.setRequestMethod(reqData.get(1));
 		
 		URI uri =new URI(req.getRequestUrl());
 		String host = uri.getHost();
-		socket = new Socket(host,80);
-		//OutputStream opt= socket.getOutputStream();
+		socket = new Socket(host, 80);
+		OutputStream opt= socket.getOutputStream();
 		
 		String pathWithoutProtocol = uri.getPath();
 		String query= uri.getQuery();
 		
-		if(query !=null && pathWithoutProtocol != null) {
+		if(pathWithoutProtocol != null && query !=null) {
 			
 				if(query.length()>0 || pathWithoutProtocol.length()>0) {
 					pathWithoutProtocol = pathWithoutProtocol + "?" +query;
 				}
 		}
 		
-		PrintWriter writer = new PrintWriter(socket.getOutputStream());
+		PrintWriter writer = new PrintWriter(opt);
 		//if condition changed
-		if(pathWithoutProtocol.length() != 0) {
-			writer.println(req.getRequestMethod().toUpperCase()+ " " + pathWithoutProtocol + " / HTTP/1.0");
+		if(pathWithoutProtocol.length() == 0) {
+			writer.println(req.getRequestMethod().toUpperCase() + " / HTTP/1.1");
 		}else {
-			writer.println(req.getRequestMethod().toUpperCase()+" / HTTP/1.0");
+			writer.println(req.getRequestMethod().toUpperCase() + " " + pathWithoutProtocol + " / HTTP/1.1");
 		}
 		
-		writer.print("Host: " + host+"\r\n");
+		writer.print("Host: " + host + "\r\n");
 		
 		// for -d inline data
 		if(req.getHasInlineData()) {
@@ -263,7 +272,7 @@ public void displayResponse(BufferedReader reader, String status) throws IOExcep
 			if (!listOfHeaders.isEmpty()) {
 
 				for (int j = 0; j < listOfHeaders.size(); j++) {
-					String headerdetails[] = listOfHeaders.get(j).split(":");
+					String[] headerdetails = listOfHeaders.get(j).split(":");
 					writer.write(headerdetails[0] + ":" + headerdetails[1] + "\r\n");
 				}
 			}
